a4f8ef3592cfe80dcc8d3c06816ef62bbde3a2e3
Chris Boyle <chris@bbck.net>
2011-03-06 01:14:31 -0500

    Added support for statistics. Going to be lazy about tests because the old xml feeds are going away.

---
+++ lib/armory/statistics.rb
@@ -0,0 +1,30 @@
+module Armory
+  class Statistics
+    attr_reader :statistics
+    
+    def initialize
+      @statistics = []
+    end
+    
+    def self.from_armory(doc)
+      Statistics.new.tap do |char|
+        doc.css("category statistic").each do |statistics|
+          char.statistics << Statistic.from_armory(statistics)
+        end
+      end
+    end
+    
+    class Statistic
+      attr_accessor :id, :name, :highest, :quantity
+      
+      def self.from_armory(doc)
+        Statistic.new.tap do |statistic|
+          statistic.id          = doc.attr('id')
+          statistic.name        = doc.attr('name')
+          statistic.highest     = doc.attr("highest").to_i
+          statistic.quantity    = doc.attr("quantity").to_i unless doc.attr("quantity") == '--'
+        end
+      end
+    end
+  end
+end
